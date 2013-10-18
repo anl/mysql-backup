79a4d85430a830abc1324755ac2be2cd4ccf6990
Andrew Leonard <andy@hurricane-ridge.com>
2013-10-17 20:28:56 -0700

    Add optional timestamp to backup files.

--- mysql-backup-all.sh
+++ mysql-backup-all.sh
@@ -7,16 +7,19 @@ set -e
 trap 'echo Exiting from error $? at line $LINENO; if [ ! -z "$dump_msgs" ] ; then echo $dump_msgs; fi' ERR
 
 function usage {
-    echo "Usage: $0 -d directory"
+    echo "Usage: $0 -d directory [-t]"
     echo "Usage: $0 -h"
     echo
-    echo " -d directory to write backups to"
+    echo " -d <directory>  path to write backups to"
+    echo " -t              append timestamp to backup filenames"
     exit 1
 }
 
-while getopts "d:h" flag; do
+append_timestamp='false'
+while getopts "d:ht" flag; do
     case $flag in
 	d) backup_dir=$OPTARG ;;
+	t) append_timestamp='true' ;;
 	*) usage ;;
     esac
 done
@@ -25,6 +28,12 @@ if [ -z "$backup_dir" ]; then
     echo "Backup directory must be specified."
     echo
     usage
+fi
+
+if [[ $append_timestamp == 'true' ]] ; then
+    timestamp=$(date +-%Y%m%d%H%M%S)
+else
+    timestamp=''
 fi
 
 if [ ! -d $backup_dir ]; then
@@ -38,7 +47,7 @@ for db in $(echo 'show databases' | mysql -Bs); do
     logger "${0}: ${db}: backup started"
     # Capture stderr of mysqldump; stdout goes to file
     # Ref. http://stackoverflow.com/questions/3130375/bash-script-store-stderr-in-variable
-    dump_msgs=$((mysqldump $db > ${backup_dir}/${db}.sql) 2>&1)
+    dump_msgs=$((mysqldump $db > ${backup_dir}/${db}${timestamp}.sql) 2>&1)
     if [ -n "$dump_msgs" ]; then
 	old_ifs=$IFS
 	# Make newline the internal field separator:
